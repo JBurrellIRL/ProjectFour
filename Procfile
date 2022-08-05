9da11830ebb0d75b1e152b362f1d5d8d1b735d9f
JBurrellIRL <jburrellirl@gmail.com>
2022-08-05 15:41:13 +0000

    Initial deployment commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pearljam_fansite.wsgi
---
+++ pearljam_fansite/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PearljamFansiteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pearljam_fansite'
