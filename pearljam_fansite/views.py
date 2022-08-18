3b3afe2261e0f71044c1e4a7ebec730e59430170
JBurrellIRL <jburrellirl@gmail.com>
2022-08-18 18:21:25 +0000

    Add crispy forms for commenting, update templates to allow commenting on reviews

--- pearljam_fansite/views.py
+++ pearljam_fansite/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class Home(generic.TemplateView):
@@ -21,4 +22,47 @@ class Reviews(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'review_detail.html'
-    
+
+    def get(self, request, slug,):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_date")
+
+        return render(
+            request,
+            "review_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_date")
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            response = redirect('/album-reviews')
+            return response
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "review_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": CommentForm()
+            },
+        )
