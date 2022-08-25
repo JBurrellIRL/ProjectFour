3847dfb83a7e3a75897df5cc508f3a25cdf91181
JBurrellIRL <jburrellirl@gmail.com>
2022-08-25 13:52:12 +0000

    Add placeholder posts for album reviews with images and placeholder text, CSS styling of home and blog page for mobile responsiveness

--- pearljam_fansite/views.py
+++ pearljam_fansite/views.py
@@ -20,7 +20,7 @@ class Reviews(generic.ListView):
     """
     model = Post
     template_name = "blogpage.html"
-    queryset = Post.objects.filter(status=1).order_by('-created_date')
+    queryset = Post.objects.filter(status=1).order_by('created_date')
     paginate_by = 9
 
 
