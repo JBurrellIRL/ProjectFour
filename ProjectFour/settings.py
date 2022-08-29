02e5789c62f91367c4f03ab193f32e3d21365447
JBurrellIRL <jburrellirl@gmail.com>
2022-08-29 16:10:06 +0000

    debug off for testing

--- ProjectFour/settings.py
+++ ProjectFour/settings.py
@@ -22,7 +22,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["pearljam-fansite.herokuapp.com", "localhost"]
 
